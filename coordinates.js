967562c48ed0ff06f936463e510b0c0af3c0af8c
cantelope <135386830+cantelope42@users.noreply.github.com>
2026-05-07 13:53:15 -0700

    Add files via upload

--- coordinates.js
+++ coordinates.js
@@ -3807,9 +3807,9 @@ const GetShaderCoord = (vx, vy, vz, geometry, renderer,
     vz = ar[2]
   }
 
-  var cpx = renderer.x //+ renderer.offsetX
-  var cpy = renderer.y //+ renderer.offsetY
-  var cpz = renderer.z //+ renderer.offsetZ
+  var cpx = renderer.x - renderer.offsetX
+  var cpy = renderer.y - renderer.offsetY
+  var cpz = renderer.z - renderer.offsetZ
 
   vx += -geometry.x
   vy += geometry.y
